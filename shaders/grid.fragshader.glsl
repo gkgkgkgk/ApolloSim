48da8396ec1cf888a3797526269f8b8c66d3f9c2
Gavri Kepets <gkepets@gmail.com>
2024-03-15 15:02:22 -0400

    sync

--- shaders/grid.fragshader.glsl
+++ shaders/grid.fragshader.glsl
@@ -8,12 +8,8 @@ void main() {
 		FragColor = vec4(0.0, 0.0, 0.0, 0.25);
 	} else {
 		float fadeFactor = clamp((length(pos) - 10.0) / (20.0 - 10.0), 0.0, 1.0);
-		
-		// Interpolate the alpha value from 1.0 (fully visible) to 0.0 (fully invisible)
 		float alpha = mix(0.25, 0.0, fadeFactor);
-		
-		// Example color, modify this vec4 to the desired color
-		vec4 color = vec4(0.0, 0.0, 0.0, alpha); // Assuming white color, modify as needed
+		vec4 color = vec4(0.0, 0.0, 0.0, alpha);
 		
 		FragColor = color;
 	}
